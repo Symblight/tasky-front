9405574aa8614f8d71ce46c1f8c6e3cf0bd3d6cc
Alexey <symblight>
2019-09-06 00:35:06 +0300

    feat(join): ui integration

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,13 @@ import { renderRoutes } from "react-router-config"
 
 import { NotFound, Root } from "@features/common"
 import { projectsRoutes } from "@features/projects"
+import { joinRoutes } from "@features/join"
 
 const routes = [
   {
     path: "/",
     component: Root,
-    routes: [...projectsRoutes(), { component: NotFound }],
+    routes: [...projectsRoutes(), ...joinRoutes(), { component: NotFound }],
   },
 ]
 
